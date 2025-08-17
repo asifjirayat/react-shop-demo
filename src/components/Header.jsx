a7b6dd5bf31846f75b94fb4de82646737be4b321
Asif Jirayat <asifjirayat@gmail.com>
2025-08-18 00:53:03 +0530

    Added all components and state with props drilled down.

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { useRef } from "react";
+import CartModal from "./CartModal.jsx";
+
+const Header = ({ cart, onUpdateCartItemQuantity }) => {
+  const modal = useRef();
+
+  const cartQuantity = cart.items.length;
+
+  const handleOpenCartClick = () => modal.current.open();
+
+  let modalActions = <button>Close</button>;
+
+  if (cartQuantity > 0) {
+    modalActions = (
+      <>
+        <button>Close</button>
+        <button>Checkout</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <CartModal
+        ref={modal}
+        cartItems={cart.items}
+        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
+        title="Your Cart"
+        actions={modalActions}
+      />
+      <header id="main-header">
+        <div id="main-title">
+          <img src="logo.png" alt="Elegant Model" />
+          <h1>Elegant Context</h1>
+        </div>
+        <p>
+          <button onClick={handleOpenCartClick}>Cart({cartQuantity})</button>
+        </p>
+      </header>
+    </>
+  );
+};
+
+export default Header;
